d401a2027c3c13b73a5c64dbbc3cee27bbed1c6b
amin gorgini <Amin@DESKTOP-G6UCML9>
2017-11-06 21:59:24 +0330

    delete temp file

--- footer.php
+++
@@ -1,4 +0,0 @@
-
-<?php if ( is_active_sidebar( 'footer-widgets' ) ) : // بررسی می کند که آیا ابزارکی وجود دارد یا خیر ?>
-		<?php dynamic_sidebar( 'footer-widgets' ); // فراخوانی ابزارک ?>
-<?php endif; ?>
--- page.php
+++
@@ -1,3 +0,0 @@
-<?php include('site-theme.php'); ?>
-
-	
